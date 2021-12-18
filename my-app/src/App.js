a8652c71ad29b9e29a4facd6d004e0b13eca8995
Chichi Huang <ziqih2@illinois.edu>
2021-12-17 22:20:04 -0600

    add style

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,10 +4,16 @@ import './App.css';
 import {Button} from "@material-ui/core";
 
 function App() {
-  const [tmp, setTmp] = useState('');
-  function handleChange(event){ setTmp(event.target.value); console.log(tmp)}
+  const [seq1, setSeq1] = useState('');
+  const [seq2, setSeq2] = useState('');
+  const [match, setMatch] = useState('');
+  const [mismatch, setMismatch] = useState('');
+  const [gap, setGap] = useState('');
 
   async function submit() {
+    console.log(seq1);
+    console.log(seq2);
+
     await fetch('/get', {method:"POST", body:JSON.stringify({}), headers:{"content_type":"application/json"}})
     .then(res => res.json()).then( async data => {
       console.log(data);
@@ -15,18 +21,19 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <form>
-        <label>
-          Name:
-          <input type="text" value={tmp} onChange={handleChange}/>
-        </label>
-      </form>
-      <div>
-        <Button color="secondary" onClick={submit}>
-            Submit
-        </Button>
+    <div className="homeOuterContainer">
+      <div className="homeInnerContainer"> 
+      <h1 className="heading">Visulization of Hirschberg Algorithm</h1>
+          <div>
+              <input placeholder="Sequence 1" className="input" type="text" onChange={(event) => setSeq1(event.target.value)} /> 
+              <input placeholder="Sequence 2" className="input mt-20" type="text" onChange={(event) => setSeq2(event.target.value)} />
+              <input placeholder="Match" className="input mt-20" type="text" onChange={(event) => setMatch(event.target.value)} />
+              <input placeholder="Misatch" className="input mt-20" type="text" onChange={(event) => setMismatch(event.target.value)} />
+              <input placeholder="Gap" className="input mt-20" type="text" onChange={(event) => setGap(event.target.value)} />
+          </div>
+          <button className="button mt-20" type="submit" onClick={submit}>Submit</button>     
       </div>
+      
     </div>
   );
 }
